f9fae2b588c247ec100f131cba1de52dd7941ad7
KapsAlbth <kappa.albeth@student.umn.ac.id>
2026-09-04 21:35:25 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_160839_KappaAlbeth/Week01/GameStore.kt
+++ src/oop_160839_KappaAlbeth/Week01/GameStore.kt
@@ -2,6 +2,8 @@ package oop_160839_KappaAlbeth.Week01
 
 fun main() {
     val gameTittle: String = "Roblox"
-    val price: Int = 400000
+    val price: Int = 700000
 
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) 100000 else 0
