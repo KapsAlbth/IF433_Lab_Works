dce1afd3592cb5abded9ee995e19b140f39359a1
KapsAlbth <kappa.albeth@student.umn.ac.id>
2026-09-11 21:38:04 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_160839_KappaAlbeth/Week02/Loan.kt
@@ -0,0 +1,15 @@
+package oop_160839_KappaAlbeth.Week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    var loanDuration: Int = 1 // Default Argument = 1 hari
+) {
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
