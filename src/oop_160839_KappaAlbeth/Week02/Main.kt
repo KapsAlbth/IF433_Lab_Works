0cbe68077719527f54fc68376799d14ceaa01555
KapsAlbth <kappa.albeth@student.umn.ac.id>
2026-09-11 21:28:34 +0700

    week02: implement selection logic for constructors

--- src/oop_160839_KappaAlbeth/Week02/Main.kt
+++ src/oop_160839_KappaAlbeth/Week02/Main.kt
@@ -12,18 +12,29 @@ fun main() {
 
     print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
-    scanner.nextLine() // Clear buffer newline (mencegah bug terbaca baris kosong)
+    scanner.nextLine() // Clear buffer newline
 
-    // Validasi di sisi pemanggil (Main)
+    // Validasi NIM di sisi pemanggil (Main)
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-        // Program berhenti di sini untuk mahasiswa ini, tidak membuat objek
     } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine() // Consume newline
 
-        // Instansiasi Objek karena data sudah aman
-        val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
+
+            // Memanggil Primary Constructor
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+        } else if (type == 2) {
+            // Memanggil Secondary Constructor (jurusan otomatis "Non-Matriculated")
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 }
