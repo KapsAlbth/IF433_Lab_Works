b32afe52f812f599ea1ada4b7c82db2c9ed82c84
KapsAlbth <kappa.albeth@student.umn.ac.id>
2026-09-11 21:01:23 +0700

    week02: add warning validation in init block

--- src/oop_160839_KappaAlbeth/Week02/Student.kt
+++ src/oop_160839_KappaAlbeth/Week02/Student.kt
@@ -1,9 +1,20 @@
 package oop_160839_KappaAlbeth.Week02
 
-class Student (new*
+class Student (
                 val name: String,
                val nim: String,
                val major: String
 ){
-    //body class kosong
+
+}
+
+class Student (val name: String, val nim: String, var major: String) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
 }
