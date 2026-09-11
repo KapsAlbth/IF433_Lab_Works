ea6299edd2c8da1cb04fbbeb32a32da98f93a58c
KapsAlbth <kappa.albeth@student.umn.ac.id>
2026-09-11 21:17:46 +0700

    week02: create basic Student class structure

--- src/oop_160839_KappaAlbeth/Week02/Student.kt
+++ src/oop_160839_KappaAlbeth/Week02/Student.kt
@@ -1,20 +1,7 @@
-package oop_160839_KappaAlbeth.Week02
-
-class Student (
-               val name: String,
-               val nim: String,
-               val major: String
+class Student ( new*
+                val name: String,
+                val nim: String,
+                val major: String
 ){
-
-init {
-    //Validasi Sederhana: Cek panjang NIM
-            if(nim.length !=5) {
-                println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-            } else {
-                print("Masukkan Jurusan: ")
-                val major = scanner.nextline()
-
-                val s1 = Student(name, nim, major)
-                println("Status: Pendaftaran selesai.")
-            }
+    //body class kosong
 }
