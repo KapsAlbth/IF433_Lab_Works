24da4bee0bc8b742f1b58c9ef65686ab14c586b2
KapsAlbth <kappa.albeth@student.umn.ac.id>
2026-09-11 20:46:34 +0700

    week02: create basic Student class structure

---
+++ src/oop_160839_KappaAlbeth/Week02/Student.kt
@@ -0,0 +1,9 @@
+package oop_160839_KappaAlbeth.Week02
+
+class Student (new*
+                val name: String,
+               val nim: String,
+               val major: String
+){
+    //body class kosong
+}
