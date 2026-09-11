2a6d4470b29044d7bb3be0766f4f2f831a779553
KapsAlbth <kappa.albeth@student.umn.ac.id>
2026-09-11 21:53:45 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_160839_KappaAlbeth/Week02/Hero.kt
@@ -0,0 +1,23 @@
+package oop_160839_KappaAlbeth.Week02
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100 // Default argument HP = 100
+) {
+    fun attack(targetName: String) {
+        println("$name menebas $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        // Mencegah HP bernilai minus
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
