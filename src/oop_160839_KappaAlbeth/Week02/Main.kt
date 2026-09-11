cd864c1695f164fe6aa59e4dbff00b2e95941281
KapsAlbth <kappa.albeth@student.umn.ac.id>
2026-09-11 21:22:28 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_160839_KappaAlbeth/Week02/Main.kt
@@ -0,0 +1,29 @@
+package oop_160839_KappaAlbeth.Week02
+
+import Student
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+    scanner.nextLine() // Clear buffer newline (mencegah bug terbaca baris kosong)
+
+    // Validasi di sisi pemanggil (Main)
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+        // Program berhenti di sini untuk mahasiswa ini, tidak membuat objek
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        // Instansiasi Objek karena data sudah aman
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
