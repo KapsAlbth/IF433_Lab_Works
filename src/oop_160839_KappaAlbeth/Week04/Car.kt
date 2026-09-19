ba62e05e1c30938c439a5a6b5471379efcbcb85a
KapsAlbth <kappa.albeth@student.umn.ac.id>
2026-09-19 21:09:15 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_160839_KappaAlbeth/Week04/Car.kt
@@ -0,0 +1,8 @@
+package oop_160839_KappaAlbeth.Week04
+
+open class Car(brand : String, val numberOfDoors: Int) : vehicle(brand) {
+
+    fun openTrunk(){
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}
