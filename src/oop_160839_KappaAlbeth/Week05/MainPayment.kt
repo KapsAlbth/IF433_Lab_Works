42a86cd9c51fc782d9492d5d788e154ba3720ef6
KapsAlbth <kappa.albeth@student.umn.ac.id>
2026-09-26 21:32:31 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_160839_KappaAlbeth/Week05/MainPayment.kt
+++ src/oop_160839_KappaAlbeth/Week05/MainPayment.kt
@@ -5,9 +5,18 @@ fun main() {
     val card = CreditCard("JohnThor Card", 100000.0)
     val paymentMethods: List<PaymentMethod> = listOf(wallet, card)
 
-    println("--- SIMULASI PEMBAYARAN POLYMORPHISM ---")
+    println("--- SIMULASI PEMBAYARAN POLYMORPHISM & SMART CASTING ---")
 
     for (method in paymentMethods) {
         method.processPayment(75000.0)
+
+        // Smart Casting Challenge using 'is' operator
+        if (method is EWallet) {
+            println("\n[Smart Casting Detected: EWallet gagal bayar, melakukan TopUp...]")
+            method.topUp(50000.0) // Kotlin otomatis menganggap 'method' sebagai EWallet
+            println("Mencoba proses pembayaran lagi...")
+            method.processPayment(75000.0)
+        }
+        println("--------------------------------------------------")
     }
 }
